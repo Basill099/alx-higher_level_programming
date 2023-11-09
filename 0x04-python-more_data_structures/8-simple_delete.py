fafc7a9d3e99988b9f1e6310f48b96c40e6fb470
onyeka chukwuemeka <iamblackgold99@yahoo.com>
2023-11-09 04:20:32 -0800

    Python - More Data Structures

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
-def multiply_by_2(my_dict):
-    return {key: val*2 for key, val in my_dict.items()}
+def simple_delete(my_dict, key=""):
+    my_dict.pop(key, None)
+    return my_dict
